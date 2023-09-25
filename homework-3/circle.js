08150b2d6bede960a52a0b8631dcb49eb33c65ae
Görkem Yağcı <92743361+gorkemyagci@users.noreply.github.com>
2023-09-25 21:24:36 +0300

    Cirlce.js

---
+++ homework-3/circle.js
@@ -0,0 +1,8 @@
+function CircleArea(radius) {
+    const area = Math.PI * radius * radius;
+    console.log(`The area of a circle with radius ${radius} is ${area}`);
+}
+
+module.exports = {
+    CircleArea
+}
